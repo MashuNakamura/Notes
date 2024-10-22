1095f467986ec3f4e402e5abb0b3bff1700cd802
Mashu <federicomatthewpratamaa@gmail.com>
2024-10-22 17:49:37 +0700

    notepad now can be updated

--- app/src/main/java/com/okerachuchupurin/notes/NotesAdapter.kt
+++ app/src/main/java/com/okerachuchupurin/notes/NotesAdapter.kt
@@ -1,40 +1,11 @@
-//package com.okerachuchupurin.notes
-//
-//import android.content.Context
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import android.widget.TextView
-//import androidx.recyclerview.widget.RecyclerView
-//
-//class NotesAdapter(private var notes: List<Note>, context: Context) : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>{
-//
-//        class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//            val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
-//            val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
-//        }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-//        val view = LayoutInflater.from(parent.context).inflate(R.layout.notes_item, parent, false)
-//        return NoteViewHolder(view)
-//    }
-//
-//    override fun getItemCount(): Int = notes.size
-//
-//    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-//        val note = notes[position]
-//        holder.titleTextView.text = note.title
-//        holder.contentTextView.text = note.content
-//    }
-//
-//}
-
 package com.okerachuchupurin.notes
 
-import android.content.Context // Changed
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -43,6 +14,7 @@ class NotesAdapter(private var notes: List<Note>, private val context: Context) 
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
         val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
+        val updateButton: ImageView = itemView.findViewById(R.id.editButton)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
@@ -56,6 +28,13 @@ class NotesAdapter(private var notes: List<Note>, private val context: Context) 
         val note = notes[position]
         holder.titleTextView.text = note.title
         holder.contentTextView.text = note.content
+
+        holder.updateButton.setOnClickListener{
+            val intent = Intent(holder.itemView.context, UpdateNoteActivity::class.java).apply {
+                putExtra("note_id", note.id)
+            }
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
     // Metode untuk memperbarui data
